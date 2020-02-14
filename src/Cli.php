8a4660e42f62188b88cbfbfdb977b5957e97f5f5
Nikita Ionov <ionov.goo@gmail.com>
2020-02-14 17:30:24 +0300

    Ступень четыре. Без беджей. Они сделанны сразу на гитхабе

--- src/Cli.php
+++ src/Cli.php
@@ -2,19 +2,16 @@
 
 namespace BrainGames\Cli;
 
-require __DIR__ . '/../vendor/autoload.php';
+//require __DIR__ . '/../vendor/autoload.php';
 
 use function cli\line;  //подключаю нужные фунуции из библиотеки
 use function cli\prompt;
 
 function run()
 {
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
-
-
-//echo "___TEST_TEST_TEST___", PHP_EOL;
-return;
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    //echo "___TEST_TEST_TEST___", PHP_EOL;
+    return;
 }
-
