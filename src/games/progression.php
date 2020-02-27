849b056641aaa0aff8752f16a89946f2c944e647
Nikita Ionov <ionov.goo@gmail.com>
2020-02-27 19:51:58 +0300

    sworn lint

--- src/games/progression.php
+++ src/games/progression.php
@@ -4,18 +4,18 @@ namespace BrainGames\games\progression;
 
 function runProgression()
 {
-  $progr = "";
-  $k = rand(-10, 10);
-  $d = rand(2, 5);
-  $question = rand(0, 9);
-  for ($i = 0; $i < 10; $i++){
-      $k = $k + $d;
-      if ($i == $question) {
-        $progr = $progr . " " . "..";
-        $ansver = $k;
-      } else {
-        $progr = $progr . " " . $k;
-      }
-  }
-  return [trim($progr), (string)$ansver];
+    $progr = "";
+    $k = rand(-10, 10);
+    $d = rand(2, 4);
+    $question = rand(0, 9);
+    for ($i = 0; $i < 10; $i++) {
+        $k = $k + $d;
+        if ($i == $question) {
+            $progr = $progr . " " . "..";
+            $ansver = $k;
+        } else {
+            $progr = $progr . " " . $k;
+        }
+    }
+    return [trim($progr), (string)$ansver];
 }
